5b24f7da0fb00246d9121c8c6011c774dde5979b
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-07 10:26:03 +0200

    chore: create initial SwiftUI project

---
+++ FocusFlow/FocusFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowApp.swift
+//  FocusFlow
+//
+//  Created by Ngoni Katsidzira  on 7/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
