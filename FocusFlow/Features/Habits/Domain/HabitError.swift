28edcb11c06b0c944af181cc7a53b8c3317ebd6e
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-10 14:35:39 +0200

    feat: add habit domain errors

---
+++ FocusFlow/Features/Habits/Domain/HabitError.swift
@@ -0,0 +1,29 @@
+//
+//  HabitError.swift
+//  FocusFlow
+//
+//  Created by Ngoni Katsidzira  on 10/7/2026.
+//
+
+import Foundation
+
+enum HabitError: LocalizedError {
+    
+    case emptyName
+    case unableToSave
+    case unableToDelete
+    case unableToUpdateOrder
+    
+    var errorDescription: String? {
+        switch self {
+        case .emptyName:
+            "Habit name cannot be empty."
+        case .unableToSave:
+            "The habit could not be saved."
+        case .unableToDelete:
+            "The habit could not be deleted."
+        case .unableToUpdateOrder:
+            "The habit order could not be updated."
+        }
+    }
+}
